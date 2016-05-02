494eb7c691ebff612fcdab9a7cb5b0f2466c1601
nshgit <nshgithub@yandex.ru>
2016-05-02 17:32:11 +0300

    Added files via upload

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=auto',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+];
